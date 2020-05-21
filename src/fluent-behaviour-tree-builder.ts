cb99b4dabe811f0c2eb13ec41b3d8cd4ce33d610
d2ts <d2tsuser@gmail.com>
2020-05-21 22:11:43 +1000

    feat(builder): add the ability to build nodes fluently

--- src/fluent-behaviour-tree-builder.ts
+++ src/fluent-behaviour-tree-builder.ts
@@ -1,18 +1,46 @@
 import ParentNode from '@/parent-node'
 import Node from '@/node'
 import {SelectorComposite, SequenceComposite} from '@/composites'
+import {
+  ActionLeaf,
+  ActionLeafTask,
+  ConditionLeaf,
+  ConditionLeafTask,
+} from '@/leafs'
 
 export class FluentBehaviourTreeBuilder<T> {
   // Needs to be a union with undefined due to the Array.pop method.
   private currentParentNode!: ParentNode<T> | undefined
   private readonly parentNodeStack: ParentNode<T>[] = []
 
+  public action(
+    task: ActionLeaf<T> | ActionLeafTask<T>
+  ): FluentBehaviourTreeBuilder<T>
+  public action(task: ActionLeafTask<T>): FluentBehaviourTreeBuilder<T> {
+    if (task instanceof ActionLeaf) {
+      return this.registerChild(task)
+    }
+
+    return this.registerChild(new ActionLeaf(task))
+  }
+
+  public condition(
+    task: ConditionLeaf<T> | ConditionLeafTask<T>
+  ): FluentBehaviourTreeBuilder<T>
+  public condition(task: ConditionLeafTask<T>): FluentBehaviourTreeBuilder<T> {
+    if (task instanceof ConditionLeaf) {
+      return this.registerChild(task)
+    }
+
+    return this.registerChild(new ConditionLeaf(task))
+  }
+
   public sequence(): FluentBehaviourTreeBuilder<T> {
-    return this.registerParent(new SequenceComposite<T>())
+    return this.registerParent(new SequenceComposite())
   }
 
   public selector(): FluentBehaviourTreeBuilder<T> {
-    return this.registerParent(new SelectorComposite<T>())
+    return this.registerParent(new SelectorComposite())
   }
 
   public end(): FluentBehaviourTreeBuilder<T> {
@@ -25,6 +53,24 @@ export class FluentBehaviourTreeBuilder<T> {
     this.currentParentNode = this.parentNodeStack.pop()
 
     return this
+  }
+
+  public build(): Node<T> {
+    if (!this.currentParentNode) {
+      throw new ReferenceError(
+        'You must register a parent node before calling end.'
+      )
+    }
+
+    return this.currentParentNode
+  }
+
+  public createAction(task: ActionLeafTask<T>): ActionLeaf<T> {
+    return new ActionLeaf(task)
+  }
+
+  public createCondition(task: ConditionLeafTask<T>): ConditionLeaf<T> {
+    return new ConditionLeaf(task)
   }
 
   private registerChild(node: Node<T>): FluentBehaviourTreeBuilder<T> {
