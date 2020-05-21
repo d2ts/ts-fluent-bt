ae89a18202c69832a818a13c0137726d17796c7c
d2ts <d2tsuser@gmail.com>
2020-05-21 22:09:15 +1000

    chore(leafs): add leaf task types

--- src/leafs/action-leaf.ts
+++ src/leafs/action-leaf.ts
@@ -1,6 +1,8 @@
 import Leaf from '@/leafs/leaf'
 import {NodeState} from '@/enums/node-state.enum'
 
+export type ActionLeafTask<T> = (blackboard: T) => NodeState
+
 export class ActionLeaf<T> extends Leaf<T, NodeState> {
   tick(blackBoard: T): NodeState {
     return this.task(blackBoard)
