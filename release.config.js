e33164e8bc509d6c8f628132f7e931ddf9c6325a
d2ts <d2tsuser@gmail.com>
2020-05-21 20:09:48 +1000

    fix(release): fix new line character in release commit message

--- release.config.js
+++ release.config.js
@@ -16,7 +16,7 @@ module.exports = {
       '@semantic-release/git',
       {
         message:
-          'chore(release): ${nextRelease.version} [skip ci]\\n\\n${nextRelease.notes}',
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
   ],
