67d27ca9d3baa69ee3e169474b682e95116e76f9
d2ts <d2tsuser@gmail.com>
2020-05-21 19:54:31 +1000

    test(decorator): add tests for inverter decorator

---
+++ tests/decorators/inverter-decorator.spec.ts
@@ -0,0 +1,26 @@
+import {InverterDecorator} from '@/decorators/inverter-decorator'
+import {ActionLeaf} from '@/leafs'
+import {NodeState} from '@/enums'
+import {testBlackboard} from '@test/test-blackboard'
+
+describe('InverterDecorator', () => {
+  it('should return running state when child returns running state', () => {
+    const node = new InverterDecorator(new ActionLeaf(() => NodeState.Running))
+
+    expect(node.tick(testBlackboard)).toBe(NodeState.Running)
+  })
+
+  it('should return failed state when child returns succeeded state', () => {
+    const node = new InverterDecorator(
+      new ActionLeaf(() => NodeState.Succeeded)
+    )
+
+    expect(node.tick(testBlackboard)).toBe(NodeState.Failed)
+  })
+
+  it('should return succeeded state when child returns failed state', () => {
+    const node = new InverterDecorator(new ActionLeaf(() => NodeState.Failed))
+
+    expect(node.tick(testBlackboard)).toBe(NodeState.Succeeded)
+  })
+})
