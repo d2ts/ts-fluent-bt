78865b56ee5593199fb62de4a9bbbcfab83295c1
d2ts <d2tsuser@gmail.com>
2020-05-21 22:13:01 +1000

    test(builder): add unit tests for fluent tree builder

---
+++ tests/fluent-behaviour-tree-builder.spec.ts
@@ -0,0 +1,56 @@
+import {FluentBehaviourTreeBuilder} from '@/fluent-behaviour-tree-builder'
+import {testBlackboard, TestBlackboard} from '@test/test-blackboard'
+import {NodeState} from '@/enums'
+
+describe('FluentBehaviourTreeBuilder', () => {
+  const createBuilder = (): FluentBehaviourTreeBuilder<TestBlackboard> =>
+    new FluentBehaviourTreeBuilder()
+
+  it('should throw an error when `end` is called without registering a parent node', () => {
+    const builder = createBuilder()
+
+    expect(builder.end).toThrowError()
+  })
+
+  it('should execute a sequence', () => {
+    const builder = createBuilder()
+    const mockAction = jest.fn()
+    const mockCondition = jest.fn()
+
+    mockAction.mockReturnValue(NodeState.Succeeded)
+    mockCondition.mockReturnValue(true)
+
+    const tree = builder
+      .sequence()
+      .condition(mockCondition)
+      .action(mockAction)
+      .end()
+      .build()
+
+    tree.tick(testBlackboard)
+
+    expect(mockCondition).toBeCalled()
+    expect(mockAction).toBeCalled()
+  })
+
+  it('should execute a selector', () => {
+    const builder = createBuilder()
+    const mockCondition = jest.fn()
+    const mockAction = jest.fn()
+
+    mockCondition.mockReturnValue(false)
+    mockAction.mockReturnValue(NodeState.Running)
+
+    const tree = builder
+      .selector()
+      .condition(mockCondition)
+      .action(mockAction)
+      .end()
+      .build()
+
+    tree.tick(testBlackboard)
+
+    expect(mockCondition).toBeCalled()
+    expect(mockAction).toBeCalled()
+  })
+})
