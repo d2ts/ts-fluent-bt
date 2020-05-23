59371d73e672e6c787a0c133e60ce5a5dc84768a
d2ts <d2tsuser@gmail.com>
2020-05-23 10:01:47 +1000

    test(decorator): add tests for until fail decorator

---
+++ tests/decorators/until-fail-decorator.spec.ts
@@ -0,0 +1,22 @@
+import {UntilFailDecorator} from '@/decorators/until-fail-decorator'
+import {ActionLeaf} from '@/leafs'
+import {NodeState} from '@/enums'
+import {testBlackboard} from '@test/test-blackboard'
+import each from 'jest-each'
+
+describe('UntilFailDecorator', () => {
+  it('should return succeeded state when child returns failed state', () => {
+    const node = new UntilFailDecorator(new ActionLeaf(() => NodeState.Failed))
+
+    expect(node.tick(testBlackboard)).toBe(NodeState.Succeeded)
+  })
+
+  each([NodeState.Succeeded, NodeState.Running]).it(
+    'should return running state for %s state',
+    (state) => {
+      const node = new UntilFailDecorator(new ActionLeaf(() => state))
+
+      expect(node.tick(testBlackboard)).toBe(NodeState.Running)
+    }
+  )
+})
