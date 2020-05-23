2ca487c99542ae35b4f66dd9352d00d4d27d8df8
d2ts <d2tsuser@gmail.com>
2020-05-23 18:51:12 +1000

    fix(decorator): fix logic for succeeder decorator
    Succeeder decorators should return running when its child returns a running state.

--- tests/decorators/succeeder-decorator.spec.ts
+++ tests/decorators/succeeder-decorator.spec.ts
@@ -5,7 +5,13 @@ import {NodeState} from '@/enums'
 import {testBlackboard} from '@test/test-blackboard'
 
 describe('SucceederDecorator', () => {
-  each([NodeState.Succeeded, NodeState.Failed, NodeState.Running]).it(
+  it('should return running state when child returns running state', () => {
+    const node = new SucceederDecorator(new ActionLeaf(() => NodeState.Running))
+
+    expect(node.tick(testBlackboard)).toBe(NodeState.Running)
+  })
+
+  each([NodeState.Succeeded, NodeState.Failed]).it(
     'should return succeeded state when child returns %s state',
     (state) => {
       const node = new SucceederDecorator(new ActionLeaf(() => state))
