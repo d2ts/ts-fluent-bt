35852f4ce7f5c638f47e7c9ad9a4728219389ecf
d2ts <d2tsuser@gmail.com>
2020-05-23 20:50:35 +1000

    feat(decorator): add retry decorator

---
+++ src/decorators/retry-decorator.ts
@@ -0,0 +1,38 @@
+import Decorator from '@/decorators/decorator'
+import Node from '@/node'
+import {NodeState} from '@/enums'
+
+export class RetryDecorator<T> extends Decorator<T> {
+  public static RETRY_INFINITE = -1
+  private counter = 0
+
+  constructor(private retryAmount: number, child?: Node<T>) {
+    super(child)
+  }
+
+  public tick(blackBoard: T): NodeState {
+    while (
+      this.counter < this.retryAmount ||
+      this.retryAmount === RetryDecorator.RETRY_INFINITE
+    ) {
+      const state = super.tick(blackBoard)
+
+      switch (state) {
+        case NodeState.Succeeded:
+          this.counter = 0
+
+          return NodeState.Succeeded
+        case NodeState.Failed:
+          this.counter += 1
+
+          break
+        case NodeState.Running:
+          return NodeState.Running
+      }
+    }
+
+    this.counter = 0
+
+    return NodeState.Failed
+  }
+}
